816c0920c216bd097c58a2485df30cc7b04f60f7
qiaochug <qiaochug@seas.upenn.edu>
2017-09-15 16:04:15 -0400

    load web

---
+++ pennDining/menuDetailVC.swift
@@ -0,0 +1,48 @@
+//
+//  menuDetailVC.swift
+//  pennDining
+//
+//  Created by Qiaochu Guo on 9/15/17.
+//  Copyright © 2017 pennDining. All rights reserved.
+//
+
+import UIKit
+
+class menuDetailVC: UIViewController {
+    
+    @IBOutlet var menuw: UIWebView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        let url = URL(string: "http://university-of-pennsylvania.cafebonappetit.com/cafe/1920-commons/")
+        if let unwrappedURL = url {
+            
+            let request = URLRequest(url: unwrappedURL)
+            let session = URLSession.shared
+            
+            let task = session.dataTask(with: request) { (data, response, error) in
+                
+                if error == nil {
+                    
+                    self.menuw.loadRequest(request)
+                    
+                } else {
+                    
+                    print("ERROR: \(error)")
+                    
+                }
+                
+            }
+            
+            task.resume()
+            
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
